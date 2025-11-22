4b61a8307e60e73b104839608afc7eea772f3b0c
SP1248 <sunitha.paturi@compass-usa.com>
2025-11-21 19:01:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,14 @@ ingredients_list = st.multiselect(
     , my_dataframe
     ,max_selections=5
     )
-if ingredients_list:
-    
+if ingredients_list:    
     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smooothiefroot_response.json(),use_container_width=True)
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    st.subheader(fruit_chosen + 'Nutrition Information')
+    smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+    sf_df = st.dataframe(data=smooothiefroot_response.json(),use_container_width=True)
 
 
 
